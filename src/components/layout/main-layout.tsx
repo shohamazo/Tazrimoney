2445d23969a5f82a2c3c02e450153bec5c9d1c11
shoam azoulay <10shoam82@gmail.com>
2025-10-29 09:49:49 +0000

    can we make transition forn pages smother?

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -9,6 +9,7 @@ import {
 } from '@/components/ui/sidebar';
 import { AppSidebar } from './sidebar';
 import { Header } from './header';
+import { cn } from '@/lib/utils';
 
 export function MainLayout({ children }: { children: React.ReactNode }) {
     const pathname = usePathname();
@@ -27,7 +28,9 @@ export function MainLayout({ children }: { children: React.ReactNode }) {
         </Sidebar>
         <SidebarInset>
             <Header />
-            <main className="p-4 sm:p-6 lg:p-8">{children}</main>
+            <main key={pathname} className={cn('p-4 sm:p-6 lg:p-8', 'animate-fade-in')}>
+              {children}
+            </main>
         </SidebarInset>
         </>
     );
