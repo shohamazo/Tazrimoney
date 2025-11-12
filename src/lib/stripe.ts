566877fceb8d5658a738206d0225ff8871804d85
shoam azoulay <10shoam82@gmail.com>
2025-11-12 08:32:05 +0000

    still Error: Failed to create checkout session.

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,4 +1,7 @@
 import Stripe from 'stripe';
+import { config } from 'dotenv';
+
+config(); // Load environment variables from .env file
 
 export const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
     apiVersion: '2024-06-20',
